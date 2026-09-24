28b1fdc2008681f11c75fafafd3b54d8b1547dce
Dhyan-p <dhyanpatel0411@gmail.com>
2026-09-23 19:58:09 -0600

    1st Commit of Scene Project
    Added state variables and functions. Created Background for day and night, and added Sun and Moon.

--- projects/scene/sketch.js
+++ projects/scene/sketch.js
@@ -6,10 +6,49 @@
 // - describe what you did to take this project "above and beyond"
 
 
+// State Variables
+let sceneState = "Night"; // Scene State Can be Change in Betweeen "Day" and "Night"
+let sunRadius, sunX, sunY, moonRadius, moonX, moonY;
+
+
 async function setup() {
   createCanvas(windowWidth, windowHeight);
+
+  // Set sunX and sunY Such That it Sets it Slightly Above Horizon in 2nd Quadrant
+  sunRadius = (windowHeight/20); // Get 1/20th the Radius of the Total Height
+  sunX = sunRadius;
+  sunY = ((windowHeight/2) + (sunRadius + 5));
+
+  // Set moonX and moonY Such That it Sets in 1st Quadrant
+  moonRadius = (windowHeight/25); // Moon is Always Smaller Then Sun, Therefor 1/25th the Radius of the Total Height
+  moonX = (windowWidth/2) + (windowWidth/4);
+  moonY = (windowHeight/2) - (windowHeight/4);
+
 }
 
 function draw() {
-  background(220);
+  // background(220);
+
+  drawSky();
+  drawMoonOrSun();
+}
+
+function drawSky() {
+  if (sceneState === "Day") {
+    background(135, 206, 235); // Sky Blue Color for Day
+  }
+  else if (sceneState === "Night") {
+    background(15, 20, 45); // Dark Navy Color for Night
+  }
+}
+
+function drawMoonOrSun() {
+  if (sceneState === "Day") {
+    fill("Yellow");
+    circle(sunX, sunY, sunRadius*2);
+  }
+  else if (sceneState === "Night") {
+    fill("white")
+    circle(moonX, moonY, moonRadius*2);
+  }
 }
