fc5360410494db370bed67aa8d8cd943f4a6a405
Dhyan-p <dhyanpatel0411@gmail.com>
2026-09-24 21:07:04 -0600

    2nd Commit of Scene Project
    Added Arc for sun so it changes based on mouseX position.

--- projects/scene/sketch.js
+++ projects/scene/sketch.js
@@ -7,37 +7,41 @@
 
 
 // State Variables
-let sceneState = "Night"; // Scene State Can be Change in Betweeen "Day" and "Night"
-let sunRadius, sunX, sunY, moonRadius, moonX, moonY;
-
-// Images
-let landscape1day;
+let sceneState = "Day"; // Scene State Can be Change in Betweeen "Day" and "Night"
+let sunRadius, sunX, sunY, maxSunY, minSunY, sunArcRatio, moonRadius, moonX, moonY;
+let horizonY; // Halfway of windowHeight
+let distanceFromCenter; // Measure how far mouseX is From the Center of the screen (in abs)
 
 
 async function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  // LoadImage From Assets Folder
-  landscape1day = await loadImage('/assets/images/landscape1-day.png');
-  image(landscape1day, 0, 0);
+  horizonY = windowHeight/2;
 
-  // Set sunX and sunY Such That it Sets it Slightly Above Horizon in 2nd Quadrant
+  // Set Sun Such That it Initially Sets it Slightly Above Horizon in 2nd Quadrant
   sunRadius = (windowHeight/20); // Get 1/20th the Radius of the Total Height
-  sunX = sunRadius;
-  sunY = ((windowHeight/2) + (sunRadius + 5));
+  sunX = mouseX;
+  maxSunY = sunRadius;
+  minSunY = ((horizonY) - (sunRadius + 5));
 
   // Set moonX and moonY Such That it Sets in 1st Quadrant
   moonRadius = (windowHeight/25); // Moon is Always Smaller Then Sun, Therefor 1/25th the Radius of the Total Height
   moonX = (windowWidth/2) + (windowWidth/4);
-  moonY = (windowHeight/2) - (windowHeight/4);
+  moonY = (horizonY) - (windowHeight/4);
 }
 
 function draw() {
-  // background(220);
-
   drawSky();
   drawMoonOrSun();
-  // drawLandscape();
+}
+
+function keyPressed() {
+  if (keyCode === 68) { // Key 'd'
+    sceneState = "Day";
+  }
+  if (keyCode === 78) { // Key 'n'
+    sceneState = "Night";
+  }
 }
 
 function drawSky() {
@@ -51,15 +55,15 @@ function drawSky() {
 
 function drawMoonOrSun() {
   if (sceneState === "Day") {
+    distanceFromCenter = abs(mouseX - (windowWidth/2));
+    sunArcRatio = (distanceFromCenter / (windowWidth/2)); // Convert Distance From Center Into 0.0 to 1.0 Ratio to Calculate sunY Arc Height.
+    sunY = (minSunY * sunArcRatio) + sunRadius; // Change sunY Based on distanceFromCenter and sunArcRatio
+
     fill("Yellow");
-    circle(sunX, sunY, sunRadius*2);
+    circle(mouseX, sunY, sunRadius*2);
   }
   else if (sceneState === "Night") {
-    fill("white");
+    fill("White");
     circle(moonX, moonY, moonRadius*2);
   }
 }
-
-// function drawLandscape() {
-
-// }
